4c9fb61614b73455cafd93de0ba85312903cbc47
c4710n <c4710n@users.noreply.github.com>
2024-08-16 13:02:57 +0800

    feat: add emacs-30

--- flake.nix
+++ flake.nix
@@ -7,25 +7,32 @@
   };
 
   outputs =
-    { self
-    , nixpkgs
-    , flake-utils
-    }: {
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    {
       # self: super: must be named final: prev: for `nix flake check` to be happy
       overlays = {
         default = final: prev: import ./overlays/emacs.nix final prev;
         emacs = final: prev: import ./overlays/emacs.nix final prev;
       };
-    } // flake-utils.lib.eachDefaultSystem (system: (
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ self.overlays.default ];
-        };
-      in
-      {
-        packages.emacs-unstable = pkgs.emacs-unstable;
-        packages.emacs-29 = pkgs.emacs-29;
-      }
-    ));
+    }
+    // flake-utils.lib.eachDefaultSystem (
+      system:
+      (
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ self.overlays.default ];
+          };
+        in
+        {
+          packages.emacs-unstable = pkgs.emacs-unstable;
+          packages.emacs-30 = pkgs.emacs-30;
+          packages.emacs-29 = pkgs.emacs-29;
+        }
+      )
+    );
 }
--- overlays/emacs.nix
+++ overlays/emacs.nix
@@ -1,6 +1,8 @@
 self: super:
 let
-  mkEmacs = namePrefix: repoMetaFile: patches: { ... }:
+  mkEmacs =
+    namePrefix: repoMetaFile: patches:
+    { ... }:
     let
       repoMeta = super.lib.importJSON repoMetaFile;
       fetcher =
@@ -11,126 +13,164 @@ let
         else
           throw "Unknown repo type ${repoMeta.type}";
     in
-    builtins.foldl'
-      (drv: fn: fn drv)
-      super.emacs
-      [
-        (drv: drv.override ({ srcRepo = true; }))
+    builtins.foldl' (drv: fn: fn drv) super.emacs [
+      (drv: drv.override ({ srcRepo = true; }))
 
-        (drv: drv.overrideAttrs (
-          old: {
-            name = "${namePrefix}-${repoMeta.version}";
-            inherit (repoMeta) version;
-            src = fetcher (builtins.removeAttrs repoMeta [ "type" "version" "branch" ]);
+      (
+        drv:
+        drv.overrideAttrs (old: {
+          name = "${namePrefix}-${repoMeta.version}";
+          inherit (repoMeta) version;
+          src = fetcher (
+            builtins.removeAttrs repoMeta [
+              "type"
+              "version"
+              "branch"
+            ]
+          );
 
-            patches = [ ];
+          patches = [ ];
 
-            postPatch = old.postPatch + ''
+          postPatch =
+            old.postPatch
+            + ''
               substituteInPlace lisp/loadup.el \
               --replace '(emacs-repository-get-version)' '"${repoMeta.rev}"' \
               --replace '(emacs-repository-get-branch)' '"${repoMeta.branch}"'
             '';
-          }
-        ))
+        })
+      )
 
-        # fix native compiler error
-        (drv: drv.overrideAttrs (
-          old: {
-            postPatch = old.postPatch + (super.lib.optionalString ((old ? NATIVE_FULL_AOT) || (old ? env.NATIVE_FULL_AOT))
-              (
-                let
-                  backendPath = (super.lib.concatStringsSep " "
-                    (builtins.map (x: ''\"-B${x}\"'') ([
-                      # Paths necessary so the JIT compiler finds its libraries:
-                      "${super.lib.getLib self.libgccjit}/lib"
-                      "${super.lib.getLib self.libgccjit}/lib/gcc"
-                      "${super.lib.getLib self.stdenv.cc.libc}/lib"
-                    ] ++ super.lib.optionals (self.stdenv.cc?cc.libgcc) [
-                      "${super.lib.getLib self.stdenv.cc.cc.libgcc}/lib"
-                    ] ++ [
+      # fix native compiler error
+      (
+        drv:
+        drv.overrideAttrs (old: {
+          postPatch =
+            old.postPatch
+            + (super.lib.optionalString ((old ? NATIVE_FULL_AOT) || (old ? env.NATIVE_FULL_AOT)) (
+              let
+                backendPath = (
+                  super.lib.concatStringsSep " " (
+                    builtins.map (x: ''\"-B${x}\"'') (
+                      [
+                        # Paths necessary so the JIT compiler finds its libraries:
+                        "${super.lib.getLib self.libgccjit}/lib"
+                        "${super.lib.getLib self.libgccjit}/lib/gcc"
+                        "${super.lib.getLib self.stdenv.cc.libc}/lib"
+                      ]
+                      ++ super.lib.optionals (self.stdenv.cc ? cc.libgcc) [
+                        "${super.lib.getLib self.stdenv.cc.cc.libgcc}/lib"
+                      ]
+                      ++ [
 
-                      # Executable paths necessary for compilation (ld, as):
-                      "${super.lib.getBin self.stdenv.cc.cc}/bin"
-                      "${super.lib.getBin self.stdenv.cc.bintools}/bin"
-                      "${super.lib.getBin self.stdenv.cc.bintools.bintools}/bin"
-                    ])));
-                in
-                ''
-                  substituteInPlace lisp/emacs-lisp/comp.el --replace-fail \
-                    "(defcustom comp-libgccjit-reproducer nil" \
-                    "(setq native-comp-driver-options '(${backendPath}))
-                     (defcustom comp-libgccjit-reproducer nil"
-                ''
-              ));
-          }
-        ))
+                        # Executable paths necessary for compilation (ld, as):
+                        "${super.lib.getBin self.stdenv.cc.cc}/bin"
+                        "${super.lib.getBin self.stdenv.cc.bintools}/bin"
+                        "${super.lib.getBin self.stdenv.cc.bintools.bintools}/bin"
+                      ]
+                    )
+                  )
+                );
+              in
+              ''
+                substituteInPlace lisp/emacs-lisp/comp.el --replace-fail \
+                  "(defcustom comp-libgccjit-reproducer nil" \
+                  "(setq native-comp-driver-options '(${backendPath}))
+                   (defcustom comp-libgccjit-reproducer nil"
+              ''
+            ));
+        })
+      )
 
-        # accept patches
-        (drv: drv.overrideAttrs (
-          old: {
-            patches = old.patches ++ patches;
-          }
-        ))
+      # accept patches
+      (
+        drv:
+        drv.overrideAttrs (old: {
+          patches = old.patches ++ patches;
+        })
+      )
 
-        # replace default icon
-        (drv: drv.overrideAttrs (
-          old: {
-            postInstall = old.postInstall + ''
+      # replace default icon
+      (
+        drv:
+        drv.overrideAttrs (old: {
+          postInstall =
+            old.postInstall
+            + ''
               cp ${./icons/Emacs.icns} $out/Applications/Emacs.app/Contents/Resources/Emacs.icns
             '';
-          }
-        ))
+        })
+      )
 
-        # make emacs package available on macOS only
-        (drv: drv.overrideAttrs (
-          old: {
-            meta = old.meta // {
-              platforms = super.lib.platforms.darwin;
+      # make emacs package available on macOS only
+      (
+        drv:
+        drv.overrideAttrs (old: {
+          meta = old.meta // {
+            platforms = super.lib.platforms.darwin;
+          };
+        })
+      )
+
+      # reconnect pkgs to the built emacs
+      (
+        drv:
+        let
+          result = drv.overrideAttrs (old: {
+            passthru = old.passthru // {
+              pkgs = self.emacsPackagesFor result;
             };
-          }
-        ))
-
-        # reconnect pkgs to the built emacs
-        (drv:
-          let
-            result = drv.overrideAttrs (old: {
-              passthru = old.passthru // {
-                pkgs = self.emacsPackagesFor result;
-              };
-            });
-          in
-          result
-        )
-      ];
+          });
+        in
+        result
+      )
+    ];
 in
 {
-  emacs-unstable = super.lib.makeOverridable
-    (mkEmacs "emacs-unstable" ../repos/emacs/unstable.json [
-      # patches from https://github.com/d12frosted/homebrew-emacs-plus
-      ./patches-30/fix-window-role.patch
-      ./patches-30/poll.patch
-      ./patches-30/system-appearance.patch
-      ./patches-30/round-undecorated-frame.patch
-    ])
-    {
-      withSQLite3 = true;
-      withTreeSitter = true;
-      withWebP = true;
-    };
+  emacs-unstable =
+    super.lib.makeOverridable
+      (mkEmacs "emacs-unstable" ../repos/emacs/unstable.json [
+        # patches from https://github.com/d12frosted/homebrew-emacs-plus
+        ./patches-30/fix-window-role.patch
+        ./patches-30/poll.patch
+        ./patches-30/system-appearance.patch
+        ./patches-30/round-undecorated-frame.patch
+      ])
+      {
+        withSQLite3 = true;
+        withTreeSitter = true;
+        withWebP = true;
+      };
 
-  emacs-29 = super.lib.makeOverridable
-    (mkEmacs "emacs-29" ../repos/emacs/29.json [
-      # patches from https://github.com/d12frosted/homebrew-emacs-plus
-      ./patches-29/fix-window-role.patch
-      ./patches-29/poll.patch
-      ./patches-29/system-appearance.patch
-      ./patches-29/round-undecorated-frame.patch
-    ])
-    {
-      withSQLite3 = true;
-      withTreeSitter = true;
-      withWebP = true;
-    };
+  emacs-30 =
+    super.lib.makeOverridable
+      (mkEmacs "emacs-30" ../repos/emacs/30.json [
+        # patches from https://github.com/d12frosted/homebrew-emacs-plus
+        ./patches-30/fix-window-role.patch
+        ./patches-30/poll.patch
+        ./patches-30/system-appearance.patch
+        ./patches-30/round-undecorated-frame.patch
+      ])
+      {
+        withSQLite3 = true;
+        withTreeSitter = true;
+        withWebP = true;
+      };
+
+  emacs-29 =
+    super.lib.makeOverridable
+      (mkEmacs "emacs-29" ../repos/emacs/29.json [
+        # patches from https://github.com/d12frosted/homebrew-emacs-plus
+        ./patches-29/fix-window-role.patch
+        ./patches-29/poll.patch
+        ./patches-29/system-appearance.patch
+        ./patches-29/round-undecorated-frame.patch
+      ])
+      {
+        withSQLite3 = true;
+        withTreeSitter = true;
+        withWebP = true;
+      };
 
   emacsWithPackagesFromUsePackage = import ../elisp.nix { pkgs = self; };
 
